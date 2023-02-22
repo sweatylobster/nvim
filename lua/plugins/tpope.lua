1ee0ca567dcc588d8abb20b458bcb2a446c5c79c
Max de Hoyos <max.dehoyos@gmail.com>
2023-02-21 21:28:03 -0800

    Cautiously backing up separated plugins in a functional state

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,4 +1,4 @@
-return {  
+return {
   'tpope/vim-fugitive', -- Git wrapper
   'tpope/vim-rhubarb', -- Some other git thing :help rhubarb
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically

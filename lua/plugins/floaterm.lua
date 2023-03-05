b3eedf0a465750ae01ce494c96dfc5e6c7aae722
Max de Hoyos <max.dehoyos@gmail.com>
2023-03-05 03:15:13 -0800

    TODO floaterm: toggle lazygit ^& ipython with a keybinding

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,11 +1,14 @@
 return {
   'voldikss/vim-floaterm',
   config = function()
-    vim.keymap.set('n', '<leader><esc>', ':FloatermToggle main <CR>', { desc = "Quick terminal browser" })
-    vim.keymap.set('t', '<leader><esc>', '<C-\\><C-n>:FloatermToggle main <CR>', { desc = "Quick terminal browser" })
+    vim.keymap.set('n', '<leader><esc>', ':FloatermToggle --name=main <CR>', { desc = "Quick terminal browser" })
+    vim.keymap.set('t', '<leader><esc>', '<C-\\><C-n>:FloatermToggle --name=main <CR>', { desc = "Quick terminal browser" })
     vim.keymap.set('t', '<esc>', '<C-\\><C-n>')
     -- keybind toggles a floating ipython repl buffer
     vim.keymap.set('n', '<leader>fi', ":FloatermNew --wintype='vsplit' powershell.exe -ExecutionPolicy ByPass -NoExit -Command 'C:\\Users\\maxde\\anaconda3\\shell\\condabin\\conda-hook.ps1' ; conda activate 'C:\\Users\\maxde\\anaconda3' ", { desc = "Interactive python repl" })
+    -- has to be a function just like the ipython repl i'll do tomorrow
+    vim.keymap.set('n', '<leader>gg', ":FloatermToggle --name=lazygit lazygit<CR>", { desc = "Lazygit lol" })
+    vim.keymap.set('t', '<leader>gg', "<C-\\><C-n>:FloatermToggle --name=lazygit <CR>", { desc = "Lazygit lol" })
     -- keybind sends vselected buffer contents to aforementioned ipython repl buffer
   end
 }
--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -3,4 +3,5 @@ return {
   'tpope/vim-rhubarb', -- Some other git thing :help rhubarb
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-surround', --Surround text objects with anything, really
+  'tpope/vim-repeat', --idk how to use this lol
 }

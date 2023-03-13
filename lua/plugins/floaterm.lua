074f51cfcc771f71f3221a4a50c0b0f9744aead2
Max de Hoyos <max.dehoyos@gmail.com>
2023-03-12 20:14:33 -0700

    Cleaned up implementation of Lazygit() and Ipython()

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -4,12 +4,19 @@ return {
     vim.keymap.set('n', '<leader><esc>', ':FloatermToggle --name=main <CR>', { desc = "Quick terminal browser" })
     vim.keymap.set('t', '<leader><esc>', '<C-\\><C-n>:FloatermToggle --name=main <CR>', { desc = "Quick terminal browser" })
     vim.keymap.set('x', '<leader>fs', ":'<,'>FloatermSend --name=repl <CR>")
+    --TODO: Make a keymap to send *ONE* approved of ipython input to *A* buffer
+    --NOTE: Strategy is probably to:
+    -- list and name buffer windows
+    -- filter for .py
+    -- display it in a telescope list
+    -- paste at current cursor location OR end of file OR marker
+    -- NOTE: Marker strategy would simply take the marker as argument when in floaterm
+    -- vim.keymap.set("t", "<leader>fr", "", { desc = "[F]loaterm [R]etrieve/[F]etch to buffer such-and-such"})
+    --TODO: Emend this to do several commands
+
     vim.cmd([[
     function Ipython()
-      let g:ipython_bufnr=floaterm#terminal#get_bufnr('repl')
-      let g:buflist=floaterm#buflist#gather()
-      let g:xs = count(g:buflist, g:ipython_bufnr)
-      if g:xs
+      if count(floaterm#buflist#gather(), floaterm#terminal#get_bufnr('repl'))
         :FloatermToggle repl
       else
         :FloatermNew --width=0.5 --height=0.8 --name=repl ipython --profile="nvim"
@@ -21,10 +28,7 @@ return {
 
     vim.cmd([[
     function Lazygit()
-      let g:lazygit_bufnr=floaterm#terminal#get_bufnr('lg')
-      let g:dabuflist=floaterm#buflist#gather()
-      let g:ys = count(g:dabuflist, g:lazygit_bufnr)
-      if g:ys
+      if count(floaterm#buflist#gather(), floaterm#terminal#get_bufnr('lg'))
         :FloatermToggle lg
       else
         :FloatermNew --width=0.95 --height=0.95 --name=lg lazygit
@@ -32,5 +36,7 @@ return {
     endfunction
     ]])
     vim.keymap.set('n', '<leader>gg', ":call Lazygit() <CR>", { desc = "Lazygit lol" })
+    vim.keymap.set('t', '<leader>gg', "<C-\\><C-n> :call Lazygit() <CR>", { desc = "Lazygit lol" })
+
   end
 }

46db0a5256fd56c702c6b06394413a078b70157e
Max de Hoyos <max.dehoyos@gmail.com>
2024-01-13 10:08:06 -0800

    Update treesitter.lua: Disable markdown highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
       -- Add languages to be installed here that you want installed for treesitter
       ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
 
-      highlight = { enable = true },
+      highlight = { enable = true, disable = {'markdown'} },
       indent = { enable = true, disable = { 'python' } },
       incremental_selection = {
         enable = true,

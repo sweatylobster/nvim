3c7c7f99d0f9a9ede7a643f1b6da4214f05745bd
Max de Hoyos <max.dehoyos@gmail.com>
2024-02-11 01:54:44 -0800

    Add bindings for FzfLua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,5 +1,9 @@
 return {
   'ibhagwan/fzf-lua',
   -- optional for icon support
-  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function ()
+    vim.keymap.set('n', '<leader>F', ':FzfLua <cr>')
+    vim.keymap.set('n', '<leader>cf', ':FzfLua complete_file <cr>')
+  end
 }

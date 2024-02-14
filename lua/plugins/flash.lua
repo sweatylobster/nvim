4f97e64a5e4063f0849f06cf7b1d20e3151e68e8
Max de Hoyos <max.dehoyos@gmail.com>
2024-02-14 13:14:40 -0800

    Add <leader>fw to find word and <leader>fl to find line

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,16 +1,42 @@
 return {
-  {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
-}
-}
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+    config = function ()
+      vim.keymap.set('n', '<leader>fl', function ()
+        require("flash").jump({
+          search = { mode = "search", max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = "^"
+        })
+      end,
+        {desc='[F]lash to a [l]ine'}
+      )
+      vim.keymap.set('n', '<leader>fw', function ()
+        require("flash").jump({
+          pattern = ".", -- initialize pattern with any char
+          search = {
+            mode = function(pattern)
+              -- remove leading dot
+              if pattern:sub(1, 1) == "." then
+                pattern = pattern:sub(2)
+              end
+              -- return word pattern and proper skip pattern
+              return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+            end,
+          },
+          -- select the range
+          jump = { pos = "range" },
+        })
+      end, {desc='[F]lash to a [w]ord'})
+    end
+  }

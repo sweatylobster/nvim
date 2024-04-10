302f8b686f16ad175aff7afddd7ed5f8b840ef40
Max de Hoyos <max.dehoyos@gmail.com>
2024-04-09 23:54:29 -0700

    fix alpha, remove crap

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -12,17 +12,13 @@ return {
       startify.section.top_buttons.val = {
 	-- startify.button( "s", " Search for files", ":Telescope git_files<CR>" ),
 	-- startify.button( "g", " Grep for a phrase", ":Telescope live_grep<CR>" ),
-	startify.button( "C", " Park it in ~/.config", ":cd ~/.config <CR>" ),
-	startify.button( "N", " Park it in ~/.config/nvim", ":cd ~/.config/nvim <CR>" ),
-	startify.button( "A", " Park it in ~/.aguila", ":cd $AGUILA <CR>" ),
 	startify.button( "i", " Edit init.lua", ":edit ~/.config/nvim/init.lua <CR>" ),
-	startify.button( "e", " Open nvim-tree", ":NvimTreeOpen <CR>" ),
 	startify.button( "m", " Make changes in lua/max", ":edit ~/.config/nvim/lua/max <CR>" ),
 	startify.button( "r", " Make a remap", ":edit ~/.config/nvim/lua/max/remap.lua <CR>" ),
 	startify.button( "o", " Change options", ":edit ~/.config/nvim/lua/max/options.lua <CR>" ),
 	-- startify.button( "p", " Modify plugins", ":edit ~/.config/nvim/lua/plugins <CR>" ),
 	startify.button( "a", " Change this screen", ":edit ~/.config/nvim/lua/plugins/alpha.lua <CR>" ),
-	startify.button( "z", " Start a Zettelkasten note", ":lua require('max.zettelkasten').new() <CR>" ),
+	startify.button( "z", " Start a Zettelkasten note", ":lua require('max.zettelkasten.zk').new('new-title') <CR>" ),
 	startify.button( "d", " Search aguila docs", ":lua require('telescope.builtin').find_files({cwd='$AGUILA/docs'}) <CR>" ),
       }
       -- disable MRU

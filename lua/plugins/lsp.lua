c9ea0fd12a3ae5a4176b46bd086a9d502afb09d6
Max de Hoyos <max.dehoyos@gmail.com>
2024-04-09 23:54:44 -0700

    add python lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -65,24 +65,24 @@ return{
       local servers = {
 	clangd = {},
 	jsonls = {},
-	jqls = {},
+	-- jqls = {},
 	-- gopls = {},
 	-- zls = {},
 	--
 
-	-- jedi_language_server = {},
-	-- pylsp = {
-	--   settings = {
-	--     pylsp = {
-	--       plugins = {
-	-- 	pycodestyle = {
-	-- 	  ignore = {'W391', 'E501'},
-	-- 	  maxLineLength = 300
-	-- 	}
-	--       }
-	--     }
-	--   }
-	-- },
+	jedi_language_server = {},
+	pylsp = {
+	  settings = {
+	    pylsp = {
+	      plugins = {
+		pycodestyle = {
+		  ignore = {'W391', 'E501'},
+		  maxLineLength = 300
+		}
+	      }
+	    }
+	  }
+	},
 	pyright = {
 	  disableOrganizeImports = true,
 	  python = {

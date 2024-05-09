e8f51f81bb417a23618ed2daac3c3b3d3229db19
Max de Hoyos <max.dehoyos@gmail.com>
2024-05-09 10:45:53 -0700

    change colorscheme to tokyonight-storm

--- lua/max/options.lua
+++ lua/max/options.lua
@@ -43,7 +43,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme catppuccin-mocha]]
+vim.cmd [[colorscheme tokyonight-storm]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'

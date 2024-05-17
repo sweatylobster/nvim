603c3b2e95b076dc9411921c2c1efbab7a916b3f
Max de Hoyos <max.dehoyos@gmail.com>
2024-05-17 15:59:34 -0700

    firenvim changes

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -8,18 +8,6 @@ return {
         vim.fn["firenvim#install"](0)
     end,
     config = function ()
-        
-    vim.g.firenvim_config = {
-        globalSettings = { alt = 'all' },
-        localSettings = {
-            [".*"] = {
-                cmdline = "neovim",
-                content = "text",
-                priority = 0,
-                selector = "textarea",
-                takeover = "never",
-            }
-        }
-    }
+        vim.g.firenvim_config.localSettings['.*'] = { cmdline = 'none', takeover = "always" }
     end
 }

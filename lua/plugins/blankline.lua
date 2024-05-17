fbf01e5f9d3e6ec6a3dc8ff1a2236a88ae1602a9
Max de Hoyos <max.dehoyos@gmail.com>
2024-05-17 15:58:49 -0700

    fixup blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,5 +1,10 @@
-return {
-  'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
-  main = 'ibl',
-  opts = {}
-}
+return { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+    config = function ()
+      require('ibl').setup()
+    end
+  }

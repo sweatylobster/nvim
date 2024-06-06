9f8e8e8bbc8e82b90a947cd406fe213ac761f3b6
Max de Hoyos <max.dehoyos@gmail.com>
2024-06-06 08:35:36 -0700

    change to sioyek

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,9 +1,9 @@
 return {
   "lervag/vimtex",
   config = function ()
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "sioyek"
     vim.g.tex_flavor = "lualatex"
-    -- vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_quickfix_mode = 0
     -- vim.o.conceallevel = 1  --always a terrible default; just use zathura!
     vim.g.tex_conceal='abdmg'
     -- vim.keymap.set('n', '<leader>lp', function()

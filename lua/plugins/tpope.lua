fb6621584d90c8149c372f38097a72b790f2ee98
sweatylobster <max.dehoyos@gmail.com>
2024-07-05 22:42:07 -0700

    Use nvim-surround instead of tpope

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-fugitive', -- Git wrapper
   'tpope/vim-rhubarb', -- Some other git thing :help rhubarb
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-surround', --Surround text objects with anything, really
+  -- 'tpope/vim-surround', --Surround text objects with anything, really
   'tpope/vim-repeat', --idk how to use this lol
   'tpope/vim-speeddating'
 }

c332b48bb9b35bdbe6f404d66e0cc9c527e30edc
Max de Hoyos <max.dehoyos@gmail.com>
2024-08-27 13:48:19 -0700

    chore: clean alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,12 +1,12 @@
 return {
--- Greeting on opening nvim
+  -- Greeting on opening nvim
   {'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
       local alpha = require('alpha')
       local startify = require('alpha.themes.startify')
       startify.section.header.val = {
-[[ :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) ]],
+	[[ :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) :) ]],
 
       }
       startify.section.top_buttons.val = {
@@ -16,10 +16,9 @@ return {
 	startify.button( "m", " Make changes in lua/max", ":edit ~/.config/nvim/lua/max <CR>" ),
 	startify.button( "r", " Make a remap", ":edit ~/.config/nvim/lua/max/remap.lua <CR>" ),
 	startify.button( "o", " Change options", ":edit ~/.config/nvim/lua/max/options.lua <CR>" ),
-	-- startify.button( "p", " Modify plugins", ":edit ~/.config/nvim/lua/plugins <CR>" ),
+	startify.button( "p", " Modify plugins", ":edit ~/.config/nvim/lua/plugins <CR>" ),
 	startify.button( "a", " Change this screen", ":edit ~/.config/nvim/lua/plugins/alpha.lua <CR>" ),
 	startify.button( "z", " Start a Zettelkasten note", ":lua require('max.zettelkasten.zk').new('new-title') <CR>" ),
-	startify.button( "d", " Search aguila docs", ":lua require('telescope.builtin').find_files({cwd='$AGUILA/docs'}) <CR>" ),
       }
       -- disable MRU
       -- startify.section.mru.val = { { type = "padding", val = 0 } }
@@ -28,7 +27,7 @@ return {
       startify.section.footer.val = {
 	{ type = "text", val = "Youdabest." },
       }
-    alpha.setup(startify.config)
+      alpha.setup(startify.config)
     end
   },
 }

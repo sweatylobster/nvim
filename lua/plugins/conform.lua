06b7ce3d039322b2598e34ce02e9b138089a7e4d
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-20 03:58:39 -0700

    add conform.lua

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      css = { "prettier" },
+      html = { "prettier" },
+      javascript = { "prettier" },
+      json = { "jq" },
+      lua = { "stylua" },
+      markdown = { "prettier" },
+      nix = { "nixpkgs_fmt" },
+      python = { "black" },
+      rust = { "rustfmt" },
+      sh = { "shfmt" },
+      sql = { "pg_format", "sql_formatter" },
+      yaml = { "prettier" },
+      zig = { "zigfmt" },
+      ["_"] = { "trim_whitespace", "trim_newlines" },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+  },
+  init = function ()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end
+}

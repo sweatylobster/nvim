f9316d5d5cb13cde7bfeffb8ae3829e0be973174
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-20 03:57:17 -0700

    edit autocmds

--- lua/max/autocmds.lua
+++ lua/max/autocmds.lua
@@ -1,26 +1,29 @@
-local function augroup(name)
-  return vim.api.nvim_create_augroup(name, { clear = false })
+local function augroup(name, opts)
+  return vim.api.nvim_create_augroup(name, opts)
 end
 
 -- Disable line numbers in terminal mode
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
-  group = augroup("terminal"),
+  group = augroup("terminal", { clear = false }),
   callback = function ()
     vim.o.number = false
     vim.o.relativenumber = false
     vim.cmd('startinsert')
+    vim.cmd('setlocal nospell')
   end
 })
 
 vim.cmd([[au BufRead,BufNewFile *.jq setfiletype jq]])
+
+-- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+
 -- Highlight on yank 
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup('YankHighlight', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
   pattern = '*',
 })
 

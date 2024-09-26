3cc18cc6c2e6fde8f66fd414fd0442ecd71e002e
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-26 16:04:25 -0700

    copy caarlos0's lualine conf

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,30 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'tokyonight',
-      component_separators = '|',
-      section_separators = '',
+      theme = "tokyonight",
+      component_separators = "",
+      section_separators = "",
+    },
+    sections = {
+      lualine_c = {
+        "%=",
+        {
+          "filename",
+          file_status = false,
+          path = 1,
+        },
+      },
+    },
+    inactive_sections = {
+      lualine_c = {
+        "%=",
+        {
+          "filename",
+          file_status = false,
+          path = 1,
+        },
+      },
+      lualine_x = { "location" },
     },
   },
 }

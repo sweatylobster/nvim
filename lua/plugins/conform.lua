69c78727ab021e85c1439c83bd88e83834cd3bbb
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-29 01:13:48 -0700

    fix(conform): add ruff formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,40 +1,40 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>F",
-        function ()
-          require("conform").format({ async = true })
-        end,
-        mode = "",
-        desc = "Format the buffer :)",
-      }
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>F",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format the buffer :)",
     },
-	opts = {
-		formatters_by_ft = {
-			css = { "prettier" },
-			html = { "prettier" },
-			javascript = { "prettier" },
-			json = { "jq" },
-			lua = { "stylua" },
-			markdown = { "prettier" },
-			nix = { "nixpkgs_fmt" },
-			python = { "black" },
-			rust = { "rustfmt" },
-			sh = { "shfmt" },
-			sql = { "pg_format", "sql_formatter" },
-			yaml = { "prettier" },
-			zig = { "zigfmt" },
-			["_"] = { "trim_whitespace", "trim_newlines" },
-		},
-		format_on_save = {
-			lsp_fallback = true,
-			timeout_ms = 500,
-		},
-	},
-	init = function()
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+  },
+  opts = {
+    formatters_by_ft = {
+      css = { "prettier" },
+      html = { "prettier" },
+      javascript = { "prettier" },
+      json = { "jq" },
+      lua = { "stylua" },
+      markdown = { "prettier" },
+      nix = { "nixpkgs_fmt" },
+      python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+      rust = { "rustfmt" },
+      sh = { "shfmt" },
+      sql = { "pg_format", "sql_formatter" },
+      yaml = { "prettier" },
+      zig = { "zigfmt" },
+      ["_"] = { "trim_whitespace", "trim_newlines" },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }

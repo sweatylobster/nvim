b7f09408fd49f4a194c0faab88b324308fa9779e
Max de Hoyos <max.dehoyos@gmail.com>
2024-10-06 03:58:08 -0700

    style: format files

--- lua/max/autocmds.lua
+++ lua/max/autocmds.lua
@@ -5,25 +5,22 @@ end
 -- Disable line numbers in terminal mode
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
   group = augroup("terminal", { clear = false }),
-  callback = function ()
+  callback = function()
     vim.o.number = false
     vim.o.relativenumber = false
-    vim.cmd('startinsert')
-    vim.cmd('setlocal nospell')
-  end
+    vim.cmd("startinsert")
+    vim.cmd("setlocal nospell")
+  end,
 })
 
 vim.cmd([[au BufRead,BufNewFile *.jq setfiletype jq]])
 
--- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-
--- Highlight on yank 
+-- Highlight on yank
 -- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = augroup('YankHighlight', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("YankHighlight", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
-  pattern = '*',
+  pattern = "*",
 })
-
--- lua/max/capture.lua
+++ lua/max/capture.lua
@@ -1,25 +1,29 @@
 local M = {
   -- Returns stdout of a command as a string.
-  to_string = function (cmd, raw)
-    local choice = assert(io.popen(cmd, 'r'))
+  to_string = function(cmd, raw)
+    local choice = assert(io.popen(cmd, "r"))
     local s = assert(choice:read("*all"))
     choice:close()
-    if raw then return s end
-    s = s:gsub('^%s+', '')
-    s = s:gsub('%s+$', '')
-    s = s:gsub('[\n\r]+', '')
+    if raw then
+      return s
+    end
+    s = s:gsub("^%s+", "")
+    s = s:gsub("%s+$", "")
+    s = s:gsub("[\n\r]+", "")
     return s
   end,
-  -- Returns stdout of a command as a table.
+  -- Return a table with lines of stdout.
+  ---@param command string
+  ---@return table stdout-lines
   to_table = function(command)
     local a = {}
-    local f = assert(io.popen(command, 'r'))
-    assert(f:lines('a'))
+    local f = assert(io.popen(command, "r"))
+    assert(f:lines("a"))
     for l in f:lines() do
       table.insert(a, l)
     end
     return a
-  end
+  end,
 }
 
 return M

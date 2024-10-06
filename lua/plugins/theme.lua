4e2c6fb77c6ea3ba3fd0126c0c02d2fcb981dbfb
Max de Hoyos <max.dehoyos@gmail.com>
2024-10-06 03:59:07 -0700

    reorganize plugins

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,12 @@
 return {
-  'navarasu/onedark.nvim', -- Theme inspired by Atom
-  'folke/tokyonight.nvim',
+  "navarasu/onedark.nvim", -- Theme inspired by Atom
+  "folke/tokyonight.nvim",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-},
-  config = function ()
-  end
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  config = function() end,
 }
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,13 +1,10 @@
 return {
   "lervag/vimtex",
-  config = function ()
+  config = function()
     vim.g.vimtex_view_method = "sioyek"
     vim.g.tex_flavor = "lualatex"
     vim.g.vimtex_quickfix_mode = 1
     -- vim.o.conceallevel = 1  --always a terrible default; just use zathura!
-    vim.g.tex_conceal='abdmg'
-    -- vim.keymap.set('n', '<leader>lp', function()
-    --   local filepath = vim.fn.expand('%:r')..".pdf"
-    --   os.execute('lpr '..filepath) end)
-  end
+    vim.g.tex_conceal = "abdmg"
+  end,
 }

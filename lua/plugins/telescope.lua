497c548ad979303bc7b09baacddf4e7f6883249d
Max de Hoyos <max.dehoyos@gmail.com>
2024-12-10 21:21:05 -0800

    style & add <C-q> telescope send to qflist default binding

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,83 +1,103 @@
 return {
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim', 'jvgrootveld/telescope-zoxide' },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim", "jvgrootveld/telescope-zoxide" },
 
-    config = function ()
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
-    require('telescope').setup {
-      defaults = {
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
-            ['<C-j>'] = 'move_selection_next',
-            ['<C-k>'] = 'move_selection_previous',
-            ['<C-t>'] = 'file_tab',
-            -- NOTE: 
-            ['<C-e>'] = 'edit_command_line',  -- more useful for opening paths in the current buf.
-            ['<C-s>'] = 'edit_search_line',
-          },
-        },
-      },
-      extensions = {
-        zoxide = {
-          prompt_title = "[i am definitely max]",
+    config = function()
+      -- [[ Configure Telescope ]]
+      -- See `:help telescope` and `:help telescope.setup()`
+      require("telescope").setup({
+        defaults = {
           mappings = {
-            default = {
-              after_action = function(selection)
-                print("Moving to (" .. selection.z_score .. ") " .. selection.path)
-              end
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+              ["<C-j>"] = "move_selection_next",
+              ["<C-k>"] = "move_selection_previous",
+              ["<C-t>"] = "file_tab",
+              ["<C-q>"] = "send_to_qflist",
+              -- NOTE:
+              ["<C-e>"] = "edit_command_line", -- more useful for opening paths in the current buf.
+              ["<C-s>"] = "edit_search_line",
             },
-            ["<C-s>"] = {
-              before_action = function(selection) print("before c-z") end,
-              action = function(selection)
-                vim.cmd.edit(selection.path)
-              end
-            },
-            ["<C-v>"] = { action = require("telescope._extensions.zoxide.utils").create_basic_command("vsplit") },
           },
         },
-      },
-    }
-
-
-
-    -- Enable telescope fzf native, if installed
-    pcall(require('telescope').load_extension, 'fzf')
-
-    -- Enable zoxide
-    pcall(require('telescope').load_extension('zoxide'))
-
-    -- Changing this binding to zi
-    vim.keymap.set('n', '<leader>zi', ':Telescope zoxide list <CR>', { desc = "List files with telescope+zoxide" } )
-
-    -- See `:help telescope.builtin`
-    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
+        extensions = {
+          zoxide = {
+            prompt_title = "[i am definitely max]",
+            mappings = {
+              default = {
+                after_action = function(selection)
+                  print("Moving to (" .. selection.z_score .. ") " .. selection.path)
+                end,
+              },
+              ["<C-s>"] = {
+                before_action = function(selection)
+                  print("before c-z")
+                end,
+                action = function(selection)
+                  vim.cmd.edit(selection.path)
+                end,
+              },
+              ["<C-v>"] = { action = require("telescope._extensions.zoxide.utils").create_basic_command("vsplit") },
+            },
+          },
+        },
       })
-    end, { desc = '[/] Fuzzily search in current buffer]' })
 
-    vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[S]earch [B]uffer' })
-    vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sj', require('telescope.builtin').jumplist, { desc = '[S]earch [J]umplist' })
-    vim.keymap.set('n', '<leader>sc', require('telescope.builtin').commands, { desc = '[S]earch [C]ommands' })
-    vim.keymap.set('n', '<leader>st', require('telescope.builtin').treesitter, { desc = '[S]earch [T]reesitter' })
-    vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status, { desc = '[G]it [S]tatus' })
-  end,
+      -- Enable telescope fzf native, if installed
+      pcall(require("telescope").load_extension, "fzf")
+
+      -- Enable zoxide
+      pcall(require("telescope").load_extension("zoxide"))
+
+      -- Changing this binding to zi
+      vim.keymap.set("n", "<leader>zi", ":Telescope zoxide list <CR>", { desc = "List files with telescope+zoxide" })
+
+      -- See `:help telescope.builtin`
+      vim.keymap.set(
+        "n",
+        "<leader>?",
+        require("telescope.builtin").oldfiles,
+        { desc = "[?] Find recently opened files" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader><space>",
+        require("telescope.builtin").buffers,
+        { desc = "[ ] Find existing buffers" }
+      )
+      vim.keymap.set("n", "<leader>/", function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+          winblend = 16, -- was 10
+          previewer = false,
+        }))
+      end, { desc = "[/] Fuzzily search in current buffer]" })
+
+      vim.keymap.set(
+        "n",
+        "<leader>sb",
+        require("telescope.builtin").current_buffer_fuzzy_find,
+        { desc = "[S]earch [B]uffer" }
+      )
+      vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sj", require("telescope.builtin").jumplist, { desc = "[S]earch [J]umplist" })
+      vim.keymap.set("n", "<leader>sc", require("telescope.builtin").commands, { desc = "[S]earch [C]ommands" })
+      vim.keymap.set("n", "<leader>st", require("telescope.builtin").treesitter, { desc = "[S]earch [T]reesitter" })
+      vim.keymap.set("n", "<leader>gs", require("telescope.builtin").git_status, { desc = "[G]it [S]tatus" })
+    end,
   },
 
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-  }
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+  },
+}

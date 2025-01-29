1c7e6dbcc37dca28d7c8868854ec9bfcb1e33515
Max de Hoyos <max.dehoyos@gmail.com>
2025-01-28 19:38:11 -0800

    add treesitter-csv

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,96 +1,98 @@
 return {
   { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = function()
-    pcall(require('nvim-treesitter.install').update { with_sync = true })
-  end,
-  dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
-  config = function ()
-    -- [[ Configure Treesitter ]]
-    -- See `:help nvim-treesitter`
-    require('nvim-treesitter.configs').setup {
-      -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'css',
-        'go',
-        'html',
-        'json',
-        'jq',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'regex',
-        'rust',
-        'typescript',
-        'vim',
-        'yaml',},
-
-      highlight = { enable = true, disable = {'help', 'latex'} },
-      indent = { enable = true, disable = {'help', } },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<c-backspace>',
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+    end,
+    dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      -- [[ Configure Treesitter ]]
+      -- See `:help nvim-treesitter`
+      require("nvim-treesitter.configs").setup({
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = {
+          "bash",
+          "c",
+          "csv",
+          "cpp",
+          "css",
+          "go",
+          "html",
+          "json",
+          "jq",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "regex",
+          "rust",
+          "typescript",
+          "vim",
+          "yaml",
         },
-      },
-      textobjects = {
-        select = {
+
+        highlight = { enable = true, disable = { "help", "latex" } },
+        indent = { enable = true, disable = { "help" } },
+        incremental_selection = {
           enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
           keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
+            init_selection = "<c-space>",
+            node_incremental = "<c-space>",
+            scope_incremental = "<c-s>",
+            node_decremental = "<c-backspace>",
           },
         },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
           },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
           },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
           },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
-      },
-    }
+      })
 
-    -- Diagnostic keymaps
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {desc = 'Go to previous diagnostic'})
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {desc = 'Go to next diagnostic'})
-    vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, {desc = 'Open diagnostic float'})
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {desc = 'Open diagnostic location list'})
-  end,
-}
+      -- Diagnostic keymaps
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+      vim.keymap.set("n", "<leader>E", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic location list" })
+    end,
+  },
 }
